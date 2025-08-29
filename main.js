c0e11315003ba9883e4c6c966e9d4dcde5a43cbc
SaritaKoirala <nightbot1272@gmail.com>
2025-08-29 16:48:48 +0200

    Update main.js

--- main.js
+++ main.js
@@ -44,21 +44,26 @@ function scrollActive() {
 window.addEventListener("scroll", scrollActive);
 
 /*===== Skills Animation =====*/
-const skills_wrap = document.querySelector(".about-skills");
-const skills_bar = document.querySelectorAll(".progress-line");
-
-function checkScroll(el) {
-  let rect = el.getBoundingClientRect();
-  return window.innerHeight >= rect.top + el.offsetHeight;
-}
-
-function skillsEffect() {
-  if (!checkScroll(skills_wrap)) return;
-  skills_bar.forEach(skill => {
-    skill.style.width = skill.dataset.progress;
-  });
-}
-window.addEventListener("scroll", skillsEffect);
+      const skills_wrap = document.querySelector(".about-skills"),
+      skills_bar = document.querySelectorAll(".progress-line");
+      window.addEventListener("scroll", () => {
+          skillsEffect();
+      })
+      // every time we scroll checking, we exceeded the about-skills or not
+      function checkScroll(el)
+      {
+          //getting the top position of about-skills relative to view port, in other words we need to get
+          // amount of pixels between about-skills and the top edge of the window.
+          let rect = el.getBoundingClientRect();
+          // after knowing the amount of pixels between the top edge of about skills and the top edge of window
+          // now we will check we exceeded the bottom edge of about skills or not
+          if(window.innerHeight >= rect.top + el.offsetHeight) return true;
+          return false;
+      }
+      function skillsEffect()
+      {
+          if(!checkScroll(skills_wrap)) return;
+          skills_bar.forEach((skill) => (skill.style.width = skill.dataset.progress));
 
 /*===== Portfolio Item Filter main page =====*/
 
@@ -109,6 +114,7 @@ lightbox.addEventListener("click", function (event) {
     toggleLightbox();
   }
 });
+
 
 
 
